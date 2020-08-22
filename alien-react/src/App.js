89f5c32b3661b3f4f4da030660b6119bb061a879
Ricardo Russ <ricardoruss@live.com>
2020-08-21 21:28:35 -0400

    cannon ball!

--- alien-react/src/App.js
+++ alien-react/src/App.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react';
 import propTypes from 'prop-types';
-
+import Canvas from "./components/Canvas"
 class App extends Component {
   render() {
     return (
       <div className="App">
-        {this.props.message}
+        <Canvas/>
       </div>
     );
   }
