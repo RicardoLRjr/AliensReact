1ce3dd5337dd93a0bdccd5595487cdd784582eed
Ricardo Russ <ricardoruss@live.com>
2020-08-22 22:31:34 -0400

    added in score

--- alien-react/src/components/Canvas.jsx
+++ alien-react/src/components/Canvas.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
-import Sky from './Sky'
-import Ground from './Ground'
-import CannonBase from './CannonBase';
-import CannonPipe from './CannonPipe'
-import CannonBall from './CannonBall'
-import PropTypes from 'prop-types'
+import React from "react";
+import Sky from "./Sky";
+import Ground from "./Ground";
+import CannonBase from "./CannonBase";
+import CannonPipe from "./CannonPipe";
+import CannonBall from "./CannonBall";
+import CurrentScore from "./CurrentScore";
+import PropTypes from "prop-types";
+
 
 const Canvas = (props) => {
-  const viewBox = [window.innerWidth / -2, 100 - window.innerHeight, window.innerWidth, window.innerHeight];
+  const viewBox = [
+    window.innerWidth / -2,
+    100 - window.innerHeight,
+    window.innerWidth,
+    window.innerHeight,
+  ];
   return (
     <svg
       id="aliens-go-home-canvas"
@@ -15,18 +22,25 @@ const Canvas = (props) => {
       onMouseMove={props.trackMouse}
       viewBox={viewBox}
     >
-<Sky/>
-    <Ground/>
-    <CannonPipe rotation={props.angle}/>
-      <CannonBase/>
-      <CannonBall position={{x: 0, y: -100}}/>
+        <defs>
+  <filter id="shadow">
+    <feDropShadow dx="1" dy="1" stdDeviation="2" />
+  </filter>
+</defs>
+      <Sky />
+      <Ground />
+      
+      <CannonPipe rotation={props.angle} />
+      <CannonBase />
+      <CannonBall position={{ x: 0, y: -100 }} />
+      <CurrentScore score={15} />
     </svg>
   );
 };
 
 Canvas.propTypes = {
-    angle: PropTypes.number.isRequired,
-    trackMouse: PropTypes.func.isRequired
-}
+  angle: PropTypes.number.isRequired,
+  trackMouse: PropTypes.func.isRequired,
+};
 
 export default Canvas;
