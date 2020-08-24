a696035880a3d55e68a78101eaf91f7aa190f2e2
Ricardo Russ <ricardoruss@live.com>
2020-08-24 17:17:14 -0400

    flying object added

--- alien-react/src/components/Canvas.jsx
+++ alien-react/src/components/Canvas.jsx
@@ -5,8 +5,8 @@ import CannonBase from "./CannonBase";
 import CannonPipe from "./CannonPipe";
 import CannonBall from "./CannonBall";
 import CurrentScore from "./CurrentScore";
+import FlyingObject from "./FlyingObject"
 import PropTypes from "prop-types";
-
 
 const Canvas = (props) => {
   const viewBox = [
@@ -22,18 +22,20 @@ const Canvas = (props) => {
       onMouseMove={props.trackMouse}
       viewBox={viewBox}
     >
-        <defs>
-  <filter id="shadow">
-    <feDropShadow dx="1" dy="1" stdDeviation="2" />
-  </filter>
-</defs>
+      <defs>
+        <filter id="shadow">
+          <feDropShadow dx="1" dy="1" stdDeviation="2" />
+        </filter>
+      </defs>
       <Sky />
       <Ground />
-      
+
       <CannonPipe rotation={props.angle} />
       <CannonBase />
       <CannonBall position={{ x: 0, y: -100 }} />
       <CurrentScore score={15} />
+      <FlyingObject position={{x: -150, y: -300}}/>
+      <FlyingObject position={{x: 150, y: -300}}/>
     </svg>
   );
 };
