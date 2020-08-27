5dbfdef5bfef3d0241afad95dbf0502d9416de48
Ricardo Russ <ricardoruss@live.com>
2020-08-26 21:52:42 -0400

    continuing to work on socket integration

--- alien-react/src/actions/index.js
+++ alien-react/src/actions/index.js
@@ -10,3 +10,17 @@ export const START_GAME = 'START_GAME';
 export const startGame = () => ({
   type: START_GAME,
 });
+
+export const LEADERBOARD_LOADED = 'LEADERBOARD_LOADED';
+export const LOGGED_IN = 'LOGGED_IN';
+
+
+export const leaderboardLoaded = players => ({
+    type: LEADERBOARD_LOADED,
+    players,
+  });
+  
+  export const loggedIn = player => ({
+    type: LOGGED_IN,
+    player,
+  });
