e032940e0c227afd2d28ab3594f212f4ab8795a7
Ricardo Russ <ricardoruss@live.com>
2020-08-26 23:37:21 -0400

    adding server for socket.io, but broken

--- alien-react/src/App.js
+++ alien-react/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import * as Auth0 from 'auth0-web'
+import * as Auth0 from "auth0-web"
+import io from 'socket.io-client'
 import { getCanvasPosition } from "./utils/formulas";
 import Canvas from "./components/Canvas";
 
@@ -16,7 +17,43 @@ class App extends Component {
     const self = this;
     Auth0.handleAuthCallback();
     Auth0.subscribe((auth) => {
-      console.log(auth);})
+      if (!auth) return;
+
+      const playerProfile = Auth0.getProfile();
+      const currentPlayer = {
+        id: playerProfile.sub,
+        maxScore: 0,
+        name: playerProfile.name,
+        picture: playerProfile.picture
+      }
+      this.props.loggedIn(currentPlayer)
+      const socket = io('http://localhost:3001', {
+        query: `token=${Auth0.getAccessToken()}`,
+      });
+
+      let emitted = false;
+      socket.on('players', (players) => {
+        this.props.leaderboardLoaded(players);
+
+        if (emitted) return;
+        socket.emit('new-max-score', {
+          id: playerProfile.sub,
+          maxScore: 120,
+          name: playerProfile.name,
+          picture: playerProfile.picture,
+        });
+        emitted = true;
+        setTimeout(() => {
+          socket.emit('new-max-score', {
+            id: playerProfile.sub,
+            maxScore: 222,
+            name: playerProfile.name,
+            picture: playerProfile.picture,
+          });
+        }, 5000);
+      });
+    });
+
     setInterval(() => {
       self.props.moveObjects(self.canvasMousePosition);
     }, 10);
@@ -37,10 +74,12 @@ class App extends Component {
     return (
       <div className="App">
         <Canvas
-          angle={this.props.angle}
-          gameState={this.props.gameState}
-          startGame={this.props.startGame}
-          trackMouse={(event) => this.trackMouse(event)}
+           angle={this.props.angle}
+           currentPlayer={this.props.currentPlayer}
+           gameState={this.props.gameState}
+           players={this.props.players}
+           startGame={this.props.startGame}
+           trackMouse={event => (this.trackMouse(event))}
         />
       </div>
     );
@@ -66,6 +105,25 @@ App.propTypes = {
   ).isRequired,
   moveObjects: PropTypes.func.isRequired,
   startGame: PropTypes.func.isRequired,
+  currentPlayer: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    maxScore: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    picture: PropTypes.string.isRequired,
+  }),
+  leaderboardLoaded: PropTypes.func.isRequired,
+  loggedIn: PropTypes.func.isRequired,
+  players: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    maxScore: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    picture: PropTypes.string.isRequired,
+  }))
+};
+
+App.defaultProps = {
+  currentPlayer: null,
+  players: null,
 };
 
 export default App;
